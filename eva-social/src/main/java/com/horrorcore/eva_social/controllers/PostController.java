3f69776bb14e0451510c1de4a84658e095199640
Matthew Martin <matthew-martin@programmer.net>
2026-01-05 15:31:33 -0500

    Started Changes into REST

--- eva-social/src/main/java/com/horrorcore/eva_social/controllers/PostController.java
+++ eva-social/src/main/java/com/horrorcore/eva_social/controllers/PostController.java
@@ -4,44 +4,37 @@ import com.horrorcore.eva_social.entites.Post;
 import com.horrorcore.eva_social.services.PostService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+//Modify
+// /api/[reasource_name]/
+@RestController
+@RequestMapping("/api/posts")
 @AllArgsConstructor
 public class PostController {
     private PostService postService;
-
-    @GetMapping(value = {"/posts/", "/posts"})
-    public String getPostsIndex(Model model) {
-        model.addAttribute("posts", postService.getAllPosts());
-        return "posts-index";
+    //Modify
+    @GetMapping(value = {"/", ""})
+    public ResponseEntity<List<Post>> getPostsRequest() {
+        return ResponseEntity.ok(postService.getAllPosts());
     }
-
-    @GetMapping(value = {"/posts/create", "/posts/create/"})
-    public String getCreatePostPage(Model model) {
-        model.addAttribute("newPost", new Post());
-        return "post-form";
+    //Modify
+    @PostMapping(value = {"/", ""})
+    public ResponseEntity<Post> postNewStatus(@RequestBody @Valid Post post) {
+        return ResponseEntity.created(null).body(postService.createPost(post));
     }
-
-    @PostMapping(value = {"/posts/create", "/posts/create/"})
-    public String postNewStatus(@ModelAttribute("newPost") @Valid Post post, Errors errors) {
-        if(errors.hasErrors()) return "post-form";
-        postService.createPost(post);
-        return "redirect:/posts";
-    }
-
+    //modify
     @PostMapping("/posts/update/likes/{id}")
     public String updateLikes(@PathVariable long id) {
         postService.updateLikes(id);
         return "redirect:/posts";
     }
-
+    //modify
     @PostMapping("/posts/update/dislikes/{id}")
     public String updateDislikes(@PathVariable long id) {
         postService.updateDislikes(id);
